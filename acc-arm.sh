235f2d301d6de9951e15c545c6379c03bba3d50a
tyodev <deve@tyo.com.au>
2018-07-25 11:26:35 +1000

    add two android cross compiler sources

---
+++ acc-arm.sh
@@ -0,0 +1,3 @@
+export TOOLCHAIN_BIN=$ANDROID_TOOLCHAINS/arm/bin
+export CC=$TOOLCHAIN_BIN/arm-linux-androideabi-gcc   # or export CC=clang
+export CXX=$TOOLCHAIN_BIN/arm-linux-androideabi-g++  # or export CXX=clang++
---
+++ acc-x86.sh
@@ -0,0 +1,3 @@
+export TOOLCHAIN_BIN=$ANDROID_TOOLCHAINS/x86/bin
+export CC=$TOOLCHAIN_BIN/i686-linux-android-gcc   # or export CC=clang
+export CXX=$TOOLCHAIN_BIN/i686-linux-android-g++  # or export CXX=clang++
