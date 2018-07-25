9008507f01f0e8e11026d059f10282328d8113da
tyodev <deve@tyo.com.au>
2018-07-25 12:14:09 +1000

    build script for new arches

---
+++ acc-arm64.sh
@@ -0,0 +1,15 @@
+export TOOLCHAIN_BIN=$ANDROID_TOOLCHAINS/arm/bin
+
+target_host=$TOOLCHAIN_BIN/aarch64-linux-android
+export AR=$target_host-ar
+export AS=$target_host-clang
+# export CC=$target_host-clang
+# export CXX=$target_host-clang++
+export CC=$target_host-gcc
+export CXX=$target_host-g++
+export LD=$target_host-ld
+export STRIP=$target_host-strip
+
+# Tell configure what flags Android requires.
+export CFLAGS="-fPIE -fPIC"
+export LDFLAGS="-pie"
---
+++ acc-x86-64.sh
@@ -0,0 +1,15 @@
+export TOOLCHAIN_BIN=$ANDROID_TOOLCHAINS/x86/bin
+
+target_host=$TOOLCHAIN_BIN/x86_64-linux-android
+export AR=$target_host-ar
+export AS=$target_host-clang
+# export CC=$target_host-clang
+# export CXX=$target_host-clang++
+export CC=$target_host-gcc
+export CXX=$target_host-g++
+export LD=$target_host-ld
+export STRIP=$target_host-strip
+
+# Tell configure what flags Android requires.
+export CFLAGS="-fPIE -fPIC"
+export LDFLAGS="-pie"
